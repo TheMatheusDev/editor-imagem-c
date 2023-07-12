a2ba41840e672080ee55574c363010a35a19bb59
Harofry <bielrego14@gmail.com>
2023-07-12 18:28:42 -0300

    Alteração no processo de rotação de imagem

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
  * Imprime uma ajuda explicando como o programa deve ser usado.
  * @param program_name Nome do arquivo executável.
@@ -18,6 +18,70 @@ void print_usage(char *program_name) {
   printf("A imagem original será lida da entrada-padrão e a imagem transformada será enviada para a saída-padrão.\n\n");
 }
 
+int rotate() {
+
+typedef struct {
+    int r, g, b;
+} Pixel;
+
+Pixel** image;
+
+int main() {
+    char formato[6];
+    int largura, altura, cormax;
+// Teste de entrada para ver se a função desenvolvida é funcional
+    printf("Type: ");
+    scanf("%s", formato);
+    printf("largura: ");
+    scanf("%d", &largura);
+    printf("altura: ");
+    scanf("%d", &altura);
+
+    // Troca natural entre as dimensões da imagem para rotacionar 90 graus
+    int rotacaoLargura = altura;
+    int rotacaoAltura = largura;
+
+    // Etapa utikizada para alocação da memória de imagem rotacionada
+    image = malloc(rotacaoAltura * sizeof(Pixel*));
+    for (int i = 0; i < rotacaoAltura; i++) {
+        image[i] = malloc(rotacaoLargura * sizeof(Pixel));
+    }
+
+    // Leitura dos pixels da imagem pré rotação
+    printf("Informe os valores RGB dos pixels:\n");
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < largura; j++) {
+            printf("Pixel [%d][%d]:\n", i, j);
+            scanf("%d", &image[rotacaoAltura - 1 - j][i].r);
+            scanf("%d", &image[rotacaoAltura - 1 - j][i].g);
+            scanf("%d", &image[rotacaoAltura - 1 - j][i].b);
+        }
+    }
+
+    // Cabeçalho da imagem pós rotação
+    printf("\nImagem rotacionada:\n");
+    printf("Formato: %s\n", formato);
+    printf("Largura: %d\n", rotacaoLargura);
+    printf("Altura: %d\n", rotacaoAltura);
+
+    // Saída de pixels pós rotação (teste de saída para ver se o programa está funcional)
+    for (int i = 0; i < rotacaoAltura; i++) {
+        for (int j = 0; j < rotacaoLargura; j++) {
+            printf("Pixel [%d][%d]:\n", i, j);
+            printf("R: %d\n", image[i][j].r);
+            printf("G: %d\n", image[i][j].g);
+            printf("B: %d\n", image[i][j].b);
+        }
+    }
+    for (int i = 0; i < rotacaoAltura; i++) {
+        free(image[i]);
+    }
+    free(image);
+  // resquicio para teste em outra aba, lembrar de adicionar <stdio.h> e <stdlib.h>
+    return 0;
+}
+
+}
 /**
  * Função principal: ponto de partida do programa.
  */
